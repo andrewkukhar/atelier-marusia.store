fa56213a48ea61859d426cf68383c425b9c118fb
andreyrise <andrey.rise@gmail.com>
2021-12-14 12:24:26 +0200

    add new block

--- js/script.js
+++ js/script.js
@@ -51,17 +51,6 @@ if (iconMenu) {
   });
 }
 
-// const iconMenu = document.querySelector('.menu__icon');
-// const menuBody = document.querySelector('.menu__body');
-// const menuList = document.querySelector('.menu__list');
-// if (iconMenu) {
-//   iconMenu.addEventListener("click", function (e) {
-//     document.body.classList.toggle('_lock');
-//     iconMenu.classList.toggle('_active');
-//     menuBody.classList.toggle('_active');
-//     menuList.classList.toggle('_active');
-//   });
-// }
 /*$(document).ready(function () {
   $('.slider').slick({
     arrows: true,
